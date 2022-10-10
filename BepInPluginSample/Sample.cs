7587df7ee5358327150a2a94c7dce0a47ddbd542
lilly <lilly1987@naver.com>
2022-10-10 14:07:32 +0900

    초기 커밋.

---
+++ BepInPluginSample/Sample.cs
@@ -0,0 +1,239 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using flanne;
+using flanne.Player;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BepInPluginSample
+{
+    [BepInPlugin("Game.Lilly.Plugin", "Lilly", "1.0")]
+    public class Sample : BaseUnityPlugin
+    {
+        public static ManualLogSource logger;
+
+        static Harmony harmony;
+
+        public ConfigEntry<BepInEx.Configuration.KeyboardShortcut> ShowCounter;
+        public ConfigEntry<BepInEx.Configuration.KeyboardShortcut> ShowCounter2;
+
+        private ConfigEntry<bool> isGUIOn;
+        private ConfigEntry<bool> isOpen;
+        private ConfigEntry<float> uiW;
+        private ConfigEntry<float> uiH;
+
+        public int windowId = 542;
+        public Rect windowRect;
+
+        public string title = "";
+        public string windowName = ""; // 변수용 
+        public string FullName = "Plugin"; // 창 펼쳤을때
+        public string ShortName = "P"; // 접었을때
+
+        GUILayoutOption h;
+        GUILayoutOption w;
+        public Vector2 scrollPosition;
+
+        // =========================================================
+
+        private static ConfigEntry<bool> hpChg;
+        private static ConfigEntry<bool> useAmmo;
+        private static ConfigEntry<float> xpMulti;
+
+        public void Awake()
+        {
+            logger = Logger;
+            Logger.LogMessage("Awake");
+
+            ShowCounter = Config.Bind("GUI", "isGUIOnKey", new KeyboardShortcut(KeyCode.Keypad0));// 이건 단축키
+            ShowCounter2 = Config.Bind("GUI", "isOpenKey", new KeyboardShortcut(KeyCode.KeypadPeriod));// 이건 단축키
+
+            isGUIOn = Config.Bind("GUI", "isGUIOn", true);
+            isOpen = Config.Bind("GUI", "isOpen", true);
+            isOpen.SettingChanged += IsOpen_SettingChanged;
+            uiW = Config.Bind("GUI", "uiW", 300f);
+            uiH = Config.Bind("GUI", "uiH", 600f);
+
+            if (isOpen.Value)
+                windowRect = new Rect(Screen.width - 65, 0, uiW.Value, 800);
+            else
+                windowRect = new Rect(Screen.width - uiW.Value, 0, uiW.Value, 800);
+
+            IsOpen_SettingChanged(null, null);
+
+            // =========================================================
+
+            hpChg = Config.Bind("game", "hpChg", true);
+            useAmmo = Config.Bind("game", "useAmmo", true);
+            xpMulti = Config.Bind("game", "xpMulti", 2f);
+        }
+
+        public void IsOpen_SettingChanged(object sender, EventArgs e)
+        {
+            logger.LogInfo($"IsOpen_SettingChanged {isOpen.Value} , {isGUIOn.Value},{windowRect.x} ");
+            if (isOpen.Value)
+            {
+                title = ShowCounter.Value.ToString() + "," + ShowCounter2.Value.ToString();
+                h = GUILayout.Height(uiH.Value);
+                w = GUILayout.Width(uiW.Value);
+                windowName = FullName;
+                windowRect.x -= (uiW.Value - 64);
+            }
+            else
+            {
+                title = "";
+                h = GUILayout.Height(40);
+                w = GUILayout.Width(60);
+                windowName = ShortName;
+                windowRect.x += (uiW.Value - 64);
+            }
+        }
+
+
+        public void OnEnable()
+        {
+            Logger.LogWarning("OnEnable");
+            // 하모니 패치
+            try // 가급적 try 처리 해주기. 하모니 패치중에 오류나면 다른 플러그인까지 영향 미침
+            {
+                harmony = Harmony.CreateAndPatchAll(typeof(Sample));
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+
+        public void Update()
+        {
+            if (ShowCounter.Value.IsUp())// 단축키가 일치할때
+            {
+                isGUIOn.Value = !isGUIOn.Value;
+            }
+            if (ShowCounter2.Value.IsUp())// 단축키가 일치할때
+            {
+                isOpen.Value = !isOpen.Value;
+            }
+        }
+
+
+        public void OnGUI()
+        {
+            if (!isGUIOn.Value)
+                return;
+
+            // 창 나가는거 방지
+            windowRect.x = Mathf.Clamp(windowRect.x, -windowRect.width + 4, Screen.width - 4);
+            windowRect.y = Mathf.Clamp(windowRect.y, -windowRect.height + 4, Screen.height - 4);
+            windowRect = GUILayout.Window(windowId, windowRect, WindowFunction, windowName, w, h);
+        }
+
+        public virtual void WindowFunction(int id)
+        {
+            GUI.enabled = true; // 기능 클릭 가능
+
+            GUILayout.BeginHorizontal();// 가로 정렬
+                                        // 라벨 추가
+                                        //GUILayout.Label(windowName, GUILayout.Height(20));
+                                        // 안쓰는 공간이 생기더라도 다른 기능으로 꽉 채우지 않고 빈공간 만들기
+            if (isOpen.Value) GUILayout.Label(title);
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { isOpen.Value = !isOpen.Value; }
+            if (GUILayout.Button("x", GUILayout.Width(20), GUILayout.Height(20))) { isGUIOn.Value = false; }
+            GUI.changed = false;
+
+            GUILayout.EndHorizontal();// 가로 정렬 끝
+
+            if (!isOpen.Value) // 닫혔을때
+            {
+            }
+            else // 열렸을때
+            {
+                scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
+
+                // 여기에 항목 작성
+                // =========================================================
+
+                if (GUILayout.Button($"hpChg {hpChg.Value}")) { hpChg.Value = !hpChg.Value; }
+
+                if (GUILayout.Button($"useAmmo {useAmmo.Value}")) { useAmmo.Value = !useAmmo.Value; }
+
+                GUILayout.BeginHorizontal();
+                GUILayout.Label($"xp Multiplier {xpMulti.Value}");
+                if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { xpMulti.Value += 1; }
+                if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { xpMulti.Value -= 1; }
+                GUILayout.EndHorizontal();
+
+                if (PlayerController.Instance != null)
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label($"move Speed {PlayerController.Instance.movementSpeed}");
+                    if (GUILayout.Button("+", GUILayout.Width(20), GUILayout.Height(20))) { PlayerController.Instance.movementSpeed += 1; }
+                    if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { PlayerController.Instance.movementSpeed -= 1; }
+                    GUILayout.EndHorizontal();
+                    GUILayout.Label($"move Speed Modify {PlayerController.Instance.stats[StatType.MoveSpeed].Modify(PlayerController.Instance.movementSpeed)}");
+                    GUILayout.Label($"move Speed Multiplier {PlayerController.Instance.moveSpeedMultiplier}");
+                }
+                else
+                {
+                    GUILayout.Label("PlayerController null");
+                }
+
+                // =========================================================
+
+                GUILayout.EndScrollView();
+            }
+            GUI.enabled = true;
+            GUI.DragWindow(); // 창 드레그 가능하게 해줌. 마지막에만 넣어야함
+        }
+
+        public void OnDisable()
+        {
+            Logger.LogWarning("OnDisable");
+            harmony?.UnpatchSelf();
+        }
+
+        // ====================== 하모니 패치 샘플 ===================================
+
+        [HarmonyPatch(typeof(Health), "HPChange")]
+        [HarmonyPrefix]
+        public static void HPChange(Health __instance, ref int change)
+        {
+            if (hpChg.Value || PlayerController.Instance.playerHealth != __instance)
+            {
+                return;
+            }
+            logger.LogWarning($"HPChange {__instance.maxHP} , {change}");
+            change = __instance.maxHP / 2;
+        }
+
+        [HarmonyPatch(typeof(Ammo), "UseAmmo")]
+        [HarmonyPrefix]
+        public static void UseAmmo(Ammo __instance, ref int a)
+        {
+            if (useAmmo.Value)
+            {
+                return;
+            }
+            logger.LogWarning($"UseAmmo {__instance.amount} , {a}");
+            a = 0;
+        }
+
+        [HarmonyPatch(typeof(PlayerXP), "GainXP")]
+        [HarmonyPrefix]
+        public static void GainXP(ref float amount, float ___xp)//PlayerXP __instance,
+        {
+            logger.LogWarning($"GainXP {___xp} , {amount}");
+            amount *= xpMulti.Value;
+        }
+
+        // =========================================================
+    }
+}
